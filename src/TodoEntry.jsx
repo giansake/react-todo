2706dd6fc3ce6b345a385654877bfbf1469131ca
Giammarco <giammarcogaudenzi@gmail.com>
2024-04-16 16:32:58 +0200

    init framer motion layout group

--- src/TodoEntry.jsx
+++ src/TodoEntry.jsx
@@ -1,31 +1,42 @@
 import { useState } from "react";
+import { LayoutGroup, AnimatePresence, motion } from "framer-motion";
 
 const TodoEntry = ({ text = "placeholder", handleDelete, setData }) => {
   const [isSelected, setIsSelected] = useState(false);
 
   return (
     <div className="todo-row-container">
-      <div className="todo-entry">
-        <input
-          type="checkbox"
-          checked={isSelected}
-          onChange={() => setIsSelected(!isSelected)}
-        />
-        <span>{text}</span>
-      </div>
+      <LayoutGroup>
+        <motion.div layout className="todo-entry">
+          <input
+            type="checkbox"
+            checked={isSelected}
+            onChange={() => setIsSelected(!isSelected)}
+          />
+          <span>{text}</span>
+        </motion.div>
 
-      {isSelected && (
-        <div className="delete">
-          <button
-            onClick={() => {
-              setData(handleDelete(text));
-              setIsSelected(false);
-            }}
-          >
-            Delete
-          </button>
-        </div>
-      )}
+        <AnimatePresence>
+          {isSelected && (
+            <motion.div
+              layout
+              className="delete"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <button
+                onClick={() => {
+                  setData(handleDelete(text));
+                  setIsSelected(false);
+                }}
+              >
+                Delete
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </LayoutGroup>
     </div>
   );
 };
