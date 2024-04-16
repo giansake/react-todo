eb5341b67b6446dc84732c5710768d21cd99a22f
Giammarco <giammarcogaudenzi@gmail.com>
2024-04-16 12:54:49 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import TodoEntry from "./TodoEntry";
+
+import "./App.css";
+
+const placeholderData = [
+  {
+    text: "groceries",
+  },
+  {
+    text: "gym",
+  },
+  {
+    text: "go outside",
+  },
+];
+
+function App() {
+  const [data, setData] = useState(placeholderData);
+
+  function handleDelete(text) {
+    return data.filter((el) => el.text !== text);
+  }
+
+  return (
+    <div>
+      {data.map((item, index) => {
+        return (
+          <TodoEntry
+            key={index}
+            text={item.text}
+            handleDelete={handleDelete}
+            setData={setData}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoEntry.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const TodoEntry = ({ text = "placeholder", handleDelete, setData }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  return (
+    <div className="todo-row-container">
+      <div className="todo-entry">
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={() => setIsSelected(!isSelected)}
+        />
+        <span>{text}</span>
+      </div>
+
+      {isSelected && (
+        <div className="delete">
+          <button onClick={() => setData(handleDelete(text))}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TodoEntry;
